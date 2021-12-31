d70f1cbb4298a0adc89e9736c3d7f29b164bb1c9
Cole Horvat <ColeHorvat@gmail.com>
2021-12-30 17:11:11 -0800

    Added temporary resume and fixed resume link

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <div className="">
                 <div className="relative flex items-center justify-between h-16 px-5">
                     <a className="text-xl" href="">Cole Horvat</a>
-                    <a className="text-xl" href="">Resume</a>
+                    <a className="text-xl" href="/Cole_Horvat_Resume_Dec_8_2021.pdf" target="_blank">Resume</a>
                     {/* ADD DARK MODE BUTTON */}
                 </div>
             </div>
