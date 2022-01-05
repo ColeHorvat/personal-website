aa588464a86a986413183d0ed8e6994e859b8e2f
Cole Horvat <ColeHorvat@gmail.com>
2022-01-04 16:48:40 -0800

    Added animations to header and footer, fixed issue where hover button backgrounds were too dim

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
+const FOOTER_ANIM_DURATION = 0.25
+
+const footer = {
+    hidden : {opacity : 0},
+    visible : {opacity : 1}
+}
 
 function Footer(props) {
     return (
-        <div className="inset-x-0 bottom-0">
-            <div className="flex px-2 pt-2 items-center justify-center">
-                <ul className="inline-flex text-gray-600">
-                    <li className="px-5 py-2 hover:bg-gray-100 text-xl"><a href="mailto:ColeHorvat@gmail.com">Email</a></li>
-                    <li className="px-5 py-2 hover:bg-gray-100 text-xl"><a href={props.twitterUrl} target="_blank" rel="noreferrer">Twitter</a></li>
-                    <li className="px-5 py-2 hover:bg-gray-100 text-xl"><a href={props.githubUrl} target="_blank" rel="noreferrer">Github</a></li>
-                    <li className="px-5 py-2 hover:bg-gray-100 text-xl"><a href={props.linkedinUrl} target="_blank" rel="noreferrer">LinkedIn</a></li>
-                </ul>
-            </div>
-            <div className="flex px-2 items-center justify-center text-gray-500">
-                <p>Built with Next.js and TailwindCSS</p>
-            </div>
-        </div>
+        <motion.div 
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: FOOTER_ANIM_DURATION }}
+            variants={footer}
+            className="inset-x-0 bottom-0">
+            <motion.div className="flex px-2 pt-2 items-center justify-center">
+                <motion.ul className="inline-flex text-gray-600">
+                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href="mailto:ColeHorvat@gmail.com">Email</motion.a></motion.li>
+                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.twitterUrl} target="_blank" rel="noreferrer">Twitter</motion.a></motion.li>
+                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.githubUrl} target="_blank" rel="noreferrer">Github</motion.a></motion.li>
+                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.linkedinUrl} target="_blank" rel="noreferrer">LinkedIn</motion.a></motion.li>
+                </motion.ul>
+            </motion.div>
+            <motion.div className="flex px-2 items-center justify-center text-gray-500">
+                <motion.p>Built with Next.js and TailwindCSS</motion.p>
+            </motion.div>
+        </motion.div>
     )
 }
 
--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+const HEADER_ANIM_DURATION = 0.25
+
+const header = {
+    hidden : {opacity : 0},
+    visible : {opacity : 1}
+}
+
 function Header() {
     return (
-        <nav className="">
-            <div className="">
-                <div className="relative flex items-center justify-between h-16 px-5">
-                    <a className="text-xl font-medium p-2 hover:bg-gray-100" href="">Cole Horvat</a>
-                    <a className="text-xl p-2 hover:bg-gray-100" href="/Cole_Horvat_Resume_Dec_8_2021.pdf" target="_blank">Resume</a>
+        <motion.nav 
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: HEADER_ANIM_DURATION}}
+            variants={header}
+            className="">
+            <motion.div className="">
+                <motion.div className="relative flex items-center justify-between h-16 px-5">
+                    <motion.a className="text-xl font-medium p-2 hover:bg-gray-200" href="">Cole Horvat</motion.a>
+                    <motion.a className="text-xl p-2 hover:bg-gray-200" href="/Cole_Horvat_Resume_Dec_8_2021.pdf" target="_blank">Resume</motion.a>
                     {/* ADD DARK MODE BUTTON */}
-                </div>
-            </div>
-        </nav>
+                </motion.div>
+            </motion.div>
+        </motion.nav>
         
     )
 }
