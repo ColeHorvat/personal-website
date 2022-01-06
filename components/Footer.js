5cb3c18a4718fcf17af6b23bfb01bb87eeec7a97
Cole Horvat <ColeHorvat@gmail.com>
2022-01-06 11:41:26 -0800

    Fixed formatting issue on smaller devices, added tools section to grid items

--- components/Footer.js
+++ components/Footer.js
@@ -18,10 +18,10 @@ function Footer(props) {
             className="inset-x-0 bottom-0">
             <motion.div className="flex px-2 pt-2 items-center justify-center">
                 <motion.ul className="inline-flex text-gray-600">
-                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href="mailto:ColeHorvat@gmail.com">Email</motion.a></motion.li>
-                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.twitterUrl} target="_blank" rel="noreferrer">Twitter</motion.a></motion.li>
-                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.githubUrl} target="_blank" rel="noreferrer">Github</motion.a></motion.li>
-                    <motion.li className="px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.linkedinUrl} target="_blank" rel="noreferrer">LinkedIn</motion.a></motion.li>
+                    <motion.li className=" px-2 sm:px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href="mailto:ColeHorvat@gmail.com">Email</motion.a></motion.li>
+                    <motion.li className="px-2 sm:px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.twitterUrl} target="_blank" rel="noreferrer">Twitter</motion.a></motion.li>
+                    <motion.li className="px-2 sm:px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.githubUrl} target="_blank" rel="noreferrer">Github</motion.a></motion.li>
+                    <motion.li className="px-2 sm:px-5 py-2 hover:bg-gray-200 text-xl"><motion.a href={props.linkedinUrl} target="_blank" rel="noreferrer">LinkedIn</motion.a></motion.li>
                 </motion.ul>
             </motion.div>
             <motion.div className="flex px-2 items-center justify-center text-gray-500">
--- components/Strings.js
+++ components/Strings.js
@@ -5,6 +5,7 @@ const STRING_CONSTANTS = {
         isVersion : true,
         title : 'Task Timer',
         description : 'A simple task app built in Android Studio with an integrated timer record the time spent on tasks.',
+        tools : 'Android Studio, Java, XML, Figma',
         versionUrl : 'https://github.com/ColeHorvat/StudyApp-AndroidStudio',
 
     },
@@ -14,6 +15,7 @@ const STRING_CONSTANTS = {
         isWebsite : true,
         title : 'Unigrams Website',
         description : 'A website using Python and the Flask web framework for "Unigrams", a product of Kai Analytics.',
+        tools : 'Python, Flask, MySQL',
         playUrl : 'https://www.unigrams.com/',
     },
 
@@ -23,6 +25,7 @@ const STRING_CONSTANTS = {
         isVersion : true,
         title : 'Lost & Found',
         description : 'A small first-person horror game made for Global Game Jam 2021 using Unity game engine.',
+        tools : "C#, Unity",
         playUrl : 'https://rabidblueberry.itch.io/william-billiams-big-adventure',
         versionUrl : 'https://github.com/ColeHorvat/Global-Game-Jam-2021',
     },
@@ -31,6 +34,7 @@ const STRING_CONSTANTS = {
         isVersion : true,
         title : 'Java Solitaire',
         description : 'A text-based solitaire game created for my Data Structures & Algorithms course to show competency in using the Stack and Queue data structures.',
+        tools : "Java",
         versionUrl : 'https://github.com/ColeHorvat/Data-Structure-Solitaire'
     },
 
@@ -39,6 +43,7 @@ const STRING_CONSTANTS = {
         isGame : true,
         title : 'Overhaul',
         description : 'A 2-D action platformer that I created for Global Game Jam Kamloops 2020. I did the majority of the scripting, AI, combat mechanics, and built the level.',
+        tools : 'C#, Unity',
         playUrl : 'https://globalgamejam.org/2020/games/overhaul-6-0',
         versionUrl : 'https://github.com/ColeHorvat/Overhaul'
     },
